59f31ea0311c4a6556ec281d0c6f167ea5a89ceb
rival <rival.abdrakhmanov@gmail.com>
2022-09-03 22:53:22 +0200

    Add some tests

---
+++ src/dotnet/diagnostics-client-plugin.basic-tests/Counters/CounterProducerConfigurationTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using AutoFixture.NUnit3;
+using DiagnosticsClientPlugin.Counters.Producer;
+using NUnit.Framework;
+using Should;
+using static DiagnosticsClientPlugin.Common.Providers;
+
+namespace DiagnosticsClientPlugin.Basic.Tests.Counters;
+
+public class CounterProducerConfigurationTests
+{
+    [Test]
+    [AutoData]
+    public void Default_configuration(string sessionId, int refreshInterval)
+    {
+        var configuration = new CounterProducerConfiguration(sessionId, string.Empty, null, refreshInterval, 0, 0);
+
+        configuration.SessionId.ShouldEqual(sessionId);
+        configuration.RefreshInterval.ShouldEqual(refreshInterval);
+        configuration.EventPipeProviders.Count.ShouldEqual(1);
+        var provider = configuration.EventPipeProviders.Single();
+        provider.Name.ShouldEqual(SystemRuntimeProvider);
+    }
+
+    [Test]
+    [AutoData]
+    public void Counters_only_configuration(string sessionId, int refreshInterval)
+    {
+        var counterProvider = "CounterProvider";
+        var configuration = new CounterProducerConfiguration(sessionId, counterProvider, null, refreshInterval, 0, 0);
+
+        configuration.EventPipeProviders.Count.ShouldEqual(1);
+        var provider = configuration.EventPipeProviders.Single();
+        provider.Name.ShouldEqual(counterProvider);
+    }
+
+    [Test]
+    [AutoData]
+    public void Metrics_only_configuration(string sessionId, int refreshInterval)
+    {
+        var meter = "Meter";
+        var configuration = new CounterProducerConfiguration(sessionId, string.Empty, meter, refreshInterval, 0, 0);
+
+        configuration.EventPipeProviders.Count.ShouldEqual(1);
+        var provider = configuration.EventPipeProviders.Single();
+        provider.Name.ShouldEqual(SystemDiagnosticsMetricsProvider);
+    }
+
+    [Test]
+    [AutoData]
+    public void Counters_and_metrics_configuration(string sessionId, int refreshInterval)
+    {
+        var counterProvider = "CounterProvider";
+        var meter = "Meter";
+        var configuration = new CounterProducerConfiguration(sessionId, counterProvider, meter, refreshInterval, 0, 0);
+
+        configuration.EventPipeProviders.Count.ShouldEqual(2);
+        var providerNames = configuration.EventPipeProviders.Select(it => it.Name).ToList();
+        providerNames.ShouldContain(counterProvider);
+        providerNames.ShouldContain(SystemDiagnosticsMetricsProvider);
+    }
+}
--- src/dotnet/diagnostics-client-plugin/Traces/TraceProviderCollection.cs
+++ src/dotnet/diagnostics-client-plugin/Traces/TraceProviderCollection.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 using System.Linq;
 using DiagnosticsClientPlugin.Generated;
 using JetBrains.Util;
 using Microsoft.Diagnostics.NETCore.Client;
 using static DiagnosticsClientPlugin.Counters.EventPipes.EventPipeProviderFactory;
-using static Microsoft.Diagnostics.Tracing.Parsers.ClrTraceEventParser;
-using static DiagnosticsClientPlugin.Common.Providers;
 
 namespace DiagnosticsClientPlugin.Traces;
 
@@ -18,198 +15,15 @@ internal sealed class TraceProviderCollection
     internal TraceProviderCollection(string providersString, TracingProfile profile)
     {
         var providers = providersString.IsNotEmpty()
-            ? ParseProviders(providersString.AsSpan())
+            ? TraceCollectionParser.Parse(providersString.AsSpan())
             : new List<TraceProvider>();
 
-        var profileProviders = ParseProfile(profile);
+        var profileProviders = TraceProfileParser.ParseProfile(profile);
 
         MergeProviders(providers, profileProviders);
 
         EventPipeProviders = CreateTraceProviders(providers);
     }
-
-    private List<TraceProvider> ParseProviders(ReadOnlySpan<char> providersString)
-    {
-        var providers = new List<TraceProvider>();
-
-        TraceProvider provider;
-        var remaining = providersString;
-        var delimiterIndex = remaining.IndexOf(',');
-        while (delimiterIndex != -1)
-        {
-            provider = ParseProvider(remaining.Slice(0, delimiterIndex).Trim());
-            providers.Add(provider);
-            remaining = remaining.Slice(delimiterIndex + 1);
-            delimiterIndex = remaining.IndexOf(',');
-        }
-
-        provider = ParseProvider(remaining.Trim());
-        providers.Add(provider);
-
-        return providers;
-    }
-
-    private TraceProvider ParseProvider(ReadOnlySpan<char> providerString)
-    {
-        var delimiterIndex = providerString.IndexOf(':');
-        if (delimiterIndex == -1)
-        {
-            return new TraceProvider(providerString.ToString(), EventLevel.Verbose, -1);
-        }
-
-        var name = providerString.Slice(0, delimiterIndex).ToString();
-
-        var remaining = providerString.Slice(delimiterIndex + 1);
-
-        delimiterIndex = remaining.IndexOf(':');
-        var flags = ParseProviderFlags(remaining.Slice(0, delimiterIndex));
-
-        remaining = remaining.Slice(delimiterIndex + 1);
-
-        EventLevel level;
-        delimiterIndex = remaining.IndexOf(':');
-        if (delimiterIndex == -1)
-        {
-            level = ParseProviderLevel(remaining.Slice(0));
-
-            return new TraceProvider(name, level, flags);
-        }
-
-        level = ParseProviderLevel(remaining.Slice(0, delimiterIndex));
-
-        remaining = remaining.Slice(delimiterIndex + 1);
-
-        var args = ParseProviderArgs(remaining);
-
-        return new TraceProvider(name, level, flags, args);
-    }
-
-    private long ParseProviderFlags(ReadOnlySpan<char> flagsPart)
-    {
-        if (flagsPart.IsEmpty)
-        {
-            return -1;
-        }
-
-        return Convert.ToInt64(flagsPart.ToString(), 16);
-    }
-
-    private EventLevel ParseProviderLevel(ReadOnlySpan<char> levelPart)
-    {
-        if (levelPart.IsEmpty)
-        {
-            return EventLevel.Verbose;
-        }
-
-        var levelString = levelPart.ToString();
-        if (int.TryParse(levelString, out var levelValue))
-        {
-            return levelValue > (int)EventLevel.Verbose ? EventLevel.Verbose : (EventLevel)levelValue;
-        }
-
-        return levelString.ToLower() switch
-        {
-            "logalways" => EventLevel.LogAlways,
-            "critical" => EventLevel.Critical,
-            "error" => EventLevel.Error,
-            "warning" => EventLevel.Warning,
-            "informational" => EventLevel.Informational,
-            "verbose" => EventLevel.Verbose,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
-
-    private Dictionary<string, string>? ParseProviderArgs(ReadOnlySpan<char> argsString)
-    {
-        if (argsString.IsEmpty)
-        {
-            return null;
-        }
-
-        var args = new Dictionary<string, string>();
-
-        var insideArgQuote = false;
-        var argStartIndex = 0;
-        (string Key, string Value)? arg;
-
-        for (var i = 0; i < argsString.Length; i++)
-        {
-            if (insideArgQuote)
-            {
-                if (argsString[i] == '\"')
-                {
-                    insideArgQuote = false;
-                }
-
-                continue;
-            }
-
-            if (argsString[i] == '\"')
-            {
-                insideArgQuote = true;
-                continue;
-            }
-
-            if (argsString[i] != ';')
-            {
-                continue;
-            }
-
-            arg = ParseArg(argsString.Slice(argStartIndex, i - argStartIndex));
-            if (arg.HasValue)
-            {
-                args.Add(arg.Value.Key, arg.Value.Value);
-            }
-
-            argStartIndex = i + 1;
-        }
-
-        arg = ParseArg(argsString.Slice(argStartIndex));
-        if (arg.HasValue)
-        {
-            args.Add(arg.Value.Key, arg.Value.Value);
-        }
-
-        return args;
-    }
-
-    private (string Key, string Value)? ParseArg(ReadOnlySpan<char> argString)
-    {
-        var delimiterIndex = argString.IndexOf('=');
-        if (delimiterIndex == -1)
-        {
-            return null;
-        }
-
-        return (
-            argString.Slice(0, delimiterIndex).ToString(),
-            argString.Slice(delimiterIndex + 1).ToString()
-        );
-    }
-
-    private TraceProvider[] ParseProfile(TracingProfile profile) =>
-        profile switch
-        {
-            TracingProfile.None => Array.Empty<TraceProvider>(),
-            TracingProfile.CpuSampling => new[]
-            {
-                new TraceProvider(SampleProfilerProvider, EventLevel.Informational, 0xF00000000000),
-                new TraceProvider(DotNetRuntimeProvider, EventLevel.Informational, (long)Keywords.Default)
-            },
-            TracingProfile.GcVerbose => new[]
-            {
-                new TraceProvider(
-                    DotNetRuntimeProvider,
-                    EventLevel.Verbose,
-                    (long)Keywords.GC | (long)Keywords.GCHandle | (long)Keywords.Exception
-                )
-            },
-            TracingProfile.GcCollect => new[]
-            {
-                new TraceProvider(DotNetRuntimeProvider, EventLevel.Informational, (long)Keywords.GC)
-            },
-            _ => throw new ArgumentOutOfRangeException(nameof(profile), profile, null)
-        };
 
     private void MergeProviders(List<TraceProvider> providers, IReadOnlyCollection<TraceProvider> profileProviders)
     {
