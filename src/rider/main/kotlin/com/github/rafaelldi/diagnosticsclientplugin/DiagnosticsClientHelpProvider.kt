73565f8d3b2b1f16942ff6e2653d0c9c79a6b608
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2023-05-29 18:34:10 +0200

    Fix help provider

--- src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/DiagnosticsClientHelpProvider.kt
+++ src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/DiagnosticsClientHelpProvider.kt
@@ -5,8 +5,9 @@ import com.intellij.openapi.help.WebHelpProvider
 class DiagnosticsClientHelpProvider : WebHelpProvider() {
     companion object {
         const val HELP_ID_PREFIX = "com.github.rafaelldi.diagnosticsclientplugin."
-        private const val GITHUB_WIKI_URL = "https://github.com/rafaelldi/diagnostics-client-plugin/wiki"
-        private const val COUNTERS_AND_METRICS = "Counters-and-Metrics"
+        private const val HELP_DOCS = "https://rafaelldi.blog/diagnostics-client-plugin"
+        private const val MAIN = "starter-topic.html"
+        private const val COUNTERS_AND_METRICS = "counters-and-metrics.html"
         private const val TRACES = "Traces"
     }
 
@@ -15,10 +16,10 @@ class DiagnosticsClientHelpProvider : WebHelpProvider() {
             return null
         }
         return when (helpTopicId.removePrefix(HELP_ID_PREFIX)) {
-            "main" -> GITHUB_WIKI_URL
-            "counters" -> "${GITHUB_WIKI_URL}/${COUNTERS_AND_METRICS}"
-            "traces" -> "${GITHUB_WIKI_URL}/${TRACES}"
-            else -> GITHUB_WIKI_URL
+            "main" -> "${HELP_DOCS}/${MAIN}"
+            "counters" -> "${HELP_DOCS}/${COUNTERS_AND_METRICS}"
+            "traces" -> "${HELP_DOCS}/${MAIN}"
+            else -> "${HELP_DOCS}/${MAIN}"
         }
     }
 }
