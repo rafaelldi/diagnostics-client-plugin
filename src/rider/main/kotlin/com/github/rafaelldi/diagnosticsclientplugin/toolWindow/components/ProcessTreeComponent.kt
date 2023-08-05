0598285613eef8fef2bea9697741397603d22fbf
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2023-08-05 22:05:47 +0200

    Add mouse and keyboard listeners for process tree

--- src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/toolWindow/components/ProcessTreeComponent.kt
+++ src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/toolWindow/components/ProcessTreeComponent.kt
@@ -2,15 +2,23 @@ package com.github.rafaelldi.diagnosticsclientplugin.toolWindow.components
 
 import com.github.rafaelldi.diagnosticsclientplugin.model.ProcessInfo
 import com.intellij.ide.CopyProvider
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.ui.ColoredTreeCellRenderer
+import com.intellij.ui.DoubleClickListener
 import com.intellij.ui.TreeSpeedSearch
 import com.intellij.ui.treeStructure.SimpleTree
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.tree.TreeUtil
 import java.awt.datatransfer.StringSelection
+import java.awt.event.InputEvent
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
+import java.awt.event.MouseEvent
 import javax.swing.JTree
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeModel
@@ -39,6 +47,22 @@ class ProcessTreeComponent : SimpleTree(), CopyProvider {
             }
         }
 
+        object : DoubleClickListener() {
+            override fun onDoubleClick(event: MouseEvent): Boolean {
+                nodeSelected(event)
+                return true
+            }
+        }.installOn(this)
+
+        addKeyListener(object : KeyAdapter() {
+            override fun keyPressed(event: KeyEvent) {
+                val keyCode = event.keyCode
+                if (keyCode == KeyEvent.VK_ENTER || keyCode == KeyEvent.VK_SPACE) {
+                    nodeSelected(event)
+                }
+            }
+        })
+
         cellRenderer = object : ColoredTreeCellRenderer() {
             override fun customizeCellRenderer(
                 tree: JTree, value: Any?, selected: Boolean, expanded: Boolean,
@@ -49,11 +73,20 @@ class ProcessTreeComponent : SimpleTree(), CopyProvider {
                 }
             }
         }
+
         updateUI()
     }
 
     val selectedNode: ProcessTreeNode?
         get() = lastSelectedPathComponent as? ProcessTreeNode
+
+    private fun nodeSelected(event: InputEvent) {
+        val selected = selectedNode ?: return
+        if (selected is LocalRootNode) {
+            val action = ActionManager.getInstance().getAction("DiagnosticsClient.ToolWindow.Explorer.Connect")
+            ActionUtil.invokeAction(action, this, ActionPlaces.TOOLWINDOW_CONTENT, event, null)
+        }
+    }
 
     fun getLocalProcessNodes(): List<LocalProcessNode> = localRoot.children()
         .asSequence()
