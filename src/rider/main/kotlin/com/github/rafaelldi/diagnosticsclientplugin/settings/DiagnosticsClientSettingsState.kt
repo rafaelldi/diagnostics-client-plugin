e43462f3cbb0fcefde5b87f4116cdefbe201b1ca
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2023-10-29 21:39:06 +0100

    Set true by default for chart settings

--- src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/settings/DiagnosticsClientSettingsState.kt
+++ src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/settings/DiagnosticsClientSettingsState.kt
@@ -3,5 +3,5 @@ package com.github.rafaelldi.diagnosticsclientplugin.settings
 import com.intellij.openapi.components.BaseState
 
 class DiagnosticsClientSettingsState : BaseState() {
-    var showChartsOnStart by property(false)
+    var showChartsOnStart by property(true)
 }
