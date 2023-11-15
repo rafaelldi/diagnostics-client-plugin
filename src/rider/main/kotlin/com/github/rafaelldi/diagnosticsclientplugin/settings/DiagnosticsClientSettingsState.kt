2b2cb1cf50971f271278a0b7c3ceb9dcf9249dbc
Rival Abdrakhmanov <16965141+rafaelldi@users.noreply.github.com>
2023-11-15 21:47:43 +0100

    Revert "Add startup activity and dotnet 8 check"
    This reverts commit 491c77ff22b31115bdf36f3911d99701eba29da7.

--- src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/settings/DiagnosticsClientSettingsState.kt
+++ src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/settings/DiagnosticsClientSettingsState.kt
@@ -3,6 +3,5 @@ package com.github.rafaelldi.diagnosticsclientplugin.settings
 import com.intellij.openapi.components.BaseState
 
 class DiagnosticsClientSettingsState : BaseState() {
-    var connectToAgent by property(true)
     var showChartsOnStart by property(true)
 }
