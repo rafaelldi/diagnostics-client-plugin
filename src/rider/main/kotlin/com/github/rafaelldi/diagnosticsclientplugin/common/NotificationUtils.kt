186a95fc24ae18e462337a101f8d88bf22716920
Rival Abdrakhmanov <16965141+rafaelldi@users.noreply.github.com>
2023-11-15 21:47:43 +0100

    Revert "Remove unused code"
    This reverts commit 56be1a37ba4789481379f72e75b168368790a408.

--- src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/common/NotificationUtils.kt
+++ src/rider/main/kotlin/com/github/rafaelldi/diagnosticsclientplugin/common/NotificationUtils.kt
@@ -5,6 +5,19 @@ import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 
+fun exportSessionFinished(type: String, pid: Int, filePath: String, openFile: Boolean, project: Project) =
+    Notification(
+        "Diagnostics Client",
+        DiagnosticsClientBundle.message("notifications.collection.finished", type),
+        DiagnosticsClientBundle.message("notifications.session.for.process.finished", pid),
+        NotificationType.INFORMATION
+    )
+        .addAction(
+            if (openFile) OpenFileAction(filePath)
+            else RevealFileAction(filePath)
+        )
+        .notify(project)
+
 fun fileDoesNotExist(path: String, project: Project) = Notification(
     "Diagnostics Client",
     DiagnosticsClientBundle.message("notifications.file.doesnt.exist"),
